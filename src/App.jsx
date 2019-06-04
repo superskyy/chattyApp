5a95d70c59d6da7365e5cd80d2ae2aaa85a555f2
Peter <psynevskyy.com>
2019-06-04 20:14:06 +0000

    importing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+// import {currentUser} from './currentUser.jsx';
+import {Messages} from './Messages.jsx';
+
 
 class App extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {loading: true};
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <h1></h1>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,2 @@
+import React, {Component} from 'react';
+import {Messages} from './Messages.jsx';
