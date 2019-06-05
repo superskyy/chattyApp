95633f7cda6e1793adefe7a8e7fbd5cc55573651
Peter <psynevskyy.com>
2019-06-05 01:08:15 +0000

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -38,23 +38,42 @@ class App extends Component {
     };
   }
 
-componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
+	componentDidMount() {
+	  console.log("componentDidMount <App />");
+	  setTimeout(() => {
+	    console.log("Simulating incoming message");
+	    // Add a new message to the list of messages in the data store
+	    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+	    const messages = this.state.messages.concat(newMessage)
+	    // Update the state of the app component.
+	    // Calling setState will trigger a call to render() in App and all child components.
+	    this.setState({messages: messages})
+	  }, 3000);
+	}
+
+	onSubmit = evt => {
+    evt.preventDefault();
+    const messageInput = evt.target.elements.newMessage;
+    const username = evt.target.elements.username;
+    const oldMessages = this.state.messages;
+    const newMessage = [
+      ...oldMessages,
+      {
+        username: username.value,
+        content: messageInput.value
+      }
+    ];
+    this.setState({ messages: newMessage });
+    messageInput.value = "";
+  };
+
+
+
   render() {
     return (
     	<div>
     	<MessageList messages={this.state.messages}/>
-    	<ChatBar username={this.state.currentUser.name} />
+	  	<ChatBar username={this.state.currentUser.name} newMessage={this.onSubmit} /> 
     	</div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 
 function ChatBar (props) {
 		return (
+			<form onSubmit={props.newMessage}>
 			<footer className="chatbar">
-			  <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.username} />
-			  <input className="chatbar-message" placeholder="Type a message and hit ENTER"/>
-			</footer> );
+			  <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.username} name="username"/>
+			  <input className="chatbar-message" placeholder="Type a message and hit ENTER" name="newMessage"/>
+				<button type="submit">Send</button>
+			</footer>
+			</form> );
 }
 
 export default ChatBar;
