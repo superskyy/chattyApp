6a3a43af5af95f26c39345418667c3eae74f0be7
Peter <psynevskyy.com>
2019-06-05 17:40:41 +0000

    refactoring and removal of unused code

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 
 function ChatBar (props) {
 		return (
-			<form onSubmit={props.newMessage}>
+			// <form onSubmit={props.newMessage}>
 			<footer className="chatbar">
-			  <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.username} name="username"/>
-			  <input className="chatbar-message" placeholder="Type a message and hit ENTER" name="newMessage"/>
-				<button className="button" type="submit">Send</button>
+			  <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.username} name="username" id="chatbar-username"/>
+			  <input onKeyPress={props.handleMessageKeyPress} className="chatbar-message" placeholder="Type a message and hit ENTER" name="newMessage" id="chatbar-msg"/>
+
 			</footer>
-			</form> );
+			// </form> 
+			);
 }
 
 export default ChatBar;
