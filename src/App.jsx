846db65e469dae0cd1044197a065d16d781f4fec
Peter <psynevskyy.com>
2019-06-06 22:07:10 +0000

    able to change username

--- src/App.jsx
+++ src/App.jsx
@@ -7,56 +7,76 @@ class App extends Component {
     super(props);
     this.state = {
     	loading: true,
-    	currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+    	currentUser: "Anonymous", // optional. if currentUser is not defined, it means the user is Anonymous
 		  messages: []
     };
   }
 
+	CreateNewName = (event) => {
+		if(event.key == 'Enter') {
+			const nameInput = document.getElementById('chatbar-username');
+			const newUserName = nameInput.value;
+			console.log(newUserName);
+
+			const newMessage = (this.state.currentUser + " changed the username to " + newUserName);
+			const newText = {
+		  	type: "postNotification",
+		  	content: newMessage
+		  };
+		  this.socket.send(JSON.stringify(newText));
+			this.setState({ currentUser: newUserName });
+	}
+}
 
 handleMessageKeyPress = (event) => {
   if(event.key == 'Enter'){
-    var messageInput = document.getElementById('chatbar-msg');
+    const messageInput = document.getElementById('chatbar-msg');
     var msg = messageInput.value;
-
-	  var chatbarUser = document.getElementById('chatbar-username');
-	  var username = chatbarUser.value;
+	  // var chatbarUser = document.getElementById('chatbar-username');
+	  var username = this.state.currentUser;
 	  const oldMessages = this.state.messages;
-
 	  const newText = {
+	  	type: "postMessage",
 	  	username: username,
 	  	content: msg
 	  };
-
 	  const newMessage = [...oldMessages, newText];
-
     this.socket.send(JSON.stringify(newText));
-    this.setState({ messages: newMessage });
-
-    // this.socket.send(JSON.parse(newText));
     messageInput.value = "";
   }
 }
 
 componentDidMount() {
-
 	this.socket = new WebSocket('ws://localhost:3001');
-
 	this.socket.onopen = () => {
-		console.log("Welcome to Chatty");
+		console.log("Connected to server");
   	// this.socket.send('Welcome To Chatty!');
 	};
 	this.socket.onmessage = e => {
-  console.log(e.data)
-  const parsed = JSON.parse(e.data);
-  const oldMessages = this.state.messages;
-  const newText = {
-	  	username: parsed.username,
-	  	content: parsed.content
-	  };
+  	console.log(e.data)
+	  const data = JSON.parse(e.data);
+	  const oldMessages = this.state.messages;
+	  const newText = {
+		  	username: data.username,
+		  	content: data.content
+		};
 
-  const newMessage = [...oldMessages, newText];
-	// this.socket.(e.data);
-	this.setState({...this.state, messages: newMessage});
+  	const newMessage = [...oldMessages, newText];
+		// this.socket.(e.data);
+		this.setState({...this.state, messages: newMessage});
+
+		switch(data.type) {
+      case "incomingMessage":
+        // handle incoming message
+        this.state.currentUser
+        break;
+      case "incomingNotification":
+        // handle incoming notification
+        break;
+      default:
+        // show an error in the console if the message type is unknown
+        throw new Error("Unknown event type " + data.type);
+    }
 	}
 };
 
@@ -64,7 +84,7 @@ render() {
     return (
     	<div>
     	<MessageList messages={this.state.messages}/>
-	  	<ChatBar username={this.state.currentUser.name} handleMessageKeyPress={this.handleMessageKeyPress}/> 
+	  	<ChatBar username={this.state.currentUser} CreateNewName={this.CreateNewName} handleMessageKeyPress={this.handleMessageKeyPress}/> 
     	</div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,13 +2,10 @@ import React, {Component} from 'react';
 
 function ChatBar (props) {
 		return (
-			// <form onSubmit={props.newMessage}>
 			<footer className="chatbar">
-			  <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.username} name="username" id="chatbar-username"/>
+			  <input onKeyPress={props.CreateNewName} className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={props.username} name="username" id="chatbar-username"/>
 			  <input onKeyPress={props.handleMessageKeyPress} className="chatbar-message" placeholder="Type a message and hit ENTER" name="newMessage" id="chatbar-msg"/>
-
 			</footer>
-			// </form> 
 			);
 }
 
