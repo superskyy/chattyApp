8fcba81dc98e27226bd927cbf435a9b53ea6b480
Peter <psynevskyy.com>
2019-06-07 01:31:28 +0000

    updates the user count

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ class App extends Component {
 			const nameInput = document.getElementById('chatbar-username');
 			const newUserName = nameInput.value;
 			console.log(newUserName);
-
 			const newMessage = (this.state.currentUser + " changed the username to " + newUserName);
 			const newText = {
 		  	type: "postNotification",
@@ -25,68 +24,68 @@ class App extends Component {
 		  };
 		  this.socket.send(JSON.stringify(newText));
 			this.setState({ currentUser: newUserName });
+		}
 	}
-}
 
-handleMessageKeyPress = (event) => {
-  if(event.key == 'Enter'){
-    const messageInput = document.getElementById('chatbar-msg');
-    var msg = messageInput.value;
-	  // var chatbarUser = document.getElementById('chatbar-username');
-	  var username = this.state.currentUser;
-	  const oldMessages = this.state.messages;
-	  const newText = {
-	  	type: "postMessage",
-	  	username: username,
-	  	content: msg
-	  };
-	  const newMessage = [...oldMessages, newText];
-    this.socket.send(JSON.stringify(newText));
-    messageInput.value = "";
-  }
-}
+	handleMessageKeyPress = (event) => {
+	  if(event.key == 'Enter'){
+	    const messageInput = document.getElementById('chatbar-msg');
+	    const msg = messageInput.value;
+		  // const chatbarUser = document.getElementById('chatbar-username');
+		  const username = this.state.currentUser;
+		  const oldMessages = this.state.messages;
+		  const newText = {
+		  	type: "postMessage",
+		  	username: username,
+		  	content: msg
+		  };
+		  const newMessage = [...oldMessages, newText];
+	    this.socket.send(JSON.stringify(newText));
+	    messageInput.value = "";
+	  }
+	}
 
-componentDidMount() {
-	this.socket = new WebSocket('ws://localhost:3001');
-	this.socket.onopen = () => {
-		console.log("Connected to server");
-  	// this.socket.send('Welcome To Chatty!');
-	};
-	this.socket.onmessage = e => {
-  	console.log(e.data)
-	  const data = JSON.parse(e.data);
-	  const oldMessages = this.state.messages;
-	  const newText = {
-		  	username: data.username,
-		  	content: data.content
+	// getUsers = () => {
+	// 	const count = JSON.parse(this.state.size);
+	// 	this.setState({size: count});
+	// }
+
+	componentDidMount() {
+		this.socket = new WebSocket('ws://localhost:3001');
+		this.socket.onopen = () => {
+			console.log("Connected to server");
 		};
-
-  	const newMessage = [...oldMessages, newText];
-		// this.socket.(e.data);
-		this.setState({...this.state, messages: newMessage});
-
-		switch(data.type) {
-      case "incomingMessage":
-        // handle incoming message
-        this.state.currentUser
-        break;
-      case "incomingNotification":
-        // handle incoming notification
-        break;
-      default:
-        // show an error in the console if the message type is unknown
-        throw new Error("Unknown event type " + data.type);
-    }
+		this.socket.onmessage = e => {
+	  	console.log(e.data)
+		  const data = JSON.parse(e.data);
+			if (data.type === "incomingMessage") {
+			  const oldMessages = this.state.messages;
+			  const newText = {
+				  	username: data.username,
+				  	content: data.content
+				};
+				const newMessage = [...oldMessages, newText];
+				this.setState({...this.state, messages: newMessage});
+				return;
+			}
+	  	
+			if (data.type === "counter") {
+				this.setState({size: data.size});
+				return;
+			}
+			}
+	};
+	render() {
+	  return (
+	  	<div>
+	    	<nav className="navbar">
+					<a href="/" className="navbar-brand">Chatty</a>
+					<p className="users-online">{this.state.size} Users Online</p>
+				</nav>
+	    	<MessageList messages={this.state.messages}/>
+		  	<ChatBar username={this.state.currentUser} CreateNewName={this.CreateNewName} handleMessageKeyPress={this.handleMessageKeyPress}/> 
+	  	</div>
+	  );
 	}
-};
-
-render() {
-    return (
-    	<div>
-    	<MessageList messages={this.state.messages}/>
-	  	<ChatBar username={this.state.currentUser} CreateNewName={this.CreateNewName} handleMessageKeyPress={this.handleMessageKeyPress}/> 
-    	</div>
-    );
-  }
 }
 export default App;
